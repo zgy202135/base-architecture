c7bec61e0804e8dbf7c91d0cb9e92379ef5050e9
Julius <2311922868@qq.com>
2020-01-11 15:20:53 +0800

    feat(初始化)：项目初始化

---
+++ src/main/java/com/julius/base/architecture/BaseArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.julius.base.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseArchitectureApplication.class, args);
+    }
+
+}
