8f942600e2349f07884b99873b9d3642a37e824b
Julius <2311922868@qq.com>
2020-04-23 18:58:26 +0800

    feat(消息总线):新增消息总线Bus

---
+++ base-bus/src/main/java/com/julius/base/bus/BaseBusApplication.java
@@ -0,0 +1,13 @@
+package com.julius.base.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseBusApplication.class, args);
+	}
+
+}
