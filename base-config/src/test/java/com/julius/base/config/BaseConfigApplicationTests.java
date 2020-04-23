04d07b033ee42cb416e4fbd4308bb82b15d72ab2
Julius <2311922868@qq.com>
2020-04-23 15:09:46 +0800

    feat(config-server):新增分布式配置中心服务端和客户端，无动态刷新功能

---
+++ base-config/src/test/java/com/julius/base/config/BaseConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julius.base.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
