cc765139fa31308589ac9219c89f958baef0b450
Julius <2311922868@qq.com>
2020-04-25 19:02:13 +0800

    feat(用户系统)：新增用户系统服务

---
+++ base-organization/src/test/java/com/julius/base/organization/BaseOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julius.base.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
